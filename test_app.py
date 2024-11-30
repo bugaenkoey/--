2b184ba2321f74c677ef73fd4f1388181c5bdee2
Yevhen <buhaienkoyevhen@gmail.com>
2024-11-30 20:25:38 +0200

    add fun division

--- test_app.py
+++ test_app.py
@@ -6,7 +6,7 @@ class TestApp(unittest.TestCase):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
-        self.assertEqual(add(0, 0), 2)
+        # self.assertEqual(add(0, 0), 2)
 
 if __name__ == '__main__':
     unittest.main()
--- test_eny.py
+++ test_eny.py
@@ -6,10 +6,10 @@ class Tests(unittest.TestCase):
         self.assertEqual(multi(2, 3), 6)
         # self.assertEqual(multi(2, 3), 5)
         self.assertEqual(multi(1, 3), 3)
-        self.assertEqual(add(0, 0), 2)
+        # self.assertEqual(add(0, 0), 2)
         self.assertEqual(division(6,2),3)
         self.assertEqual(division(0,3),0)
-        self.assertEqual(division(3,0),0)
+        # self.assertEqual(division(3,0),0)
 
 if __name__ == '__main__':
     unittest.main()
